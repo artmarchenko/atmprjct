f8b0d5bde1514ea06a4ff340b03a57402fcfa429
Artem Marchenko <art.marchenko@gmail.com>
2018-02-14 01:01:24 +0200

    dbConnector complit

--- src/utils/dbConnector.php
+++ src/utils/dbConnector.php
@@ -8,6 +8,9 @@
 
 namespace atm;
 
+use Exception;
+use mysqli;
+
 include("../DbProperties.php");
 
 
@@ -16,8 +19,22 @@ class dbConnector extends DbProperties
 {
 
 
+    public function dbConnect()
+    {
+        $conn = new mysqli($this->server, $this->user, $this->password, $this->dbName);
 
+        if ($conn->connect_error) {
+
+            throw new Exception("Connection failed: ".$conn->connect_error);
+
+        }
+        else {
+
+            return $conn;
+        }
+    }
 
 }
+
 
     
